5a151c97cfb124b78d2f602c60ec00a9c4dc447b
Tejaswini N <nteju37@gmail.com>
2020-05-18 17:28:12 +0530

    ok done

--- Memu/app/src/main/java/com/memu/etc/APIs.kt
+++ Memu/app/src/main/java/com/memu/etc/APIs.kt
@@ -4,7 +4,7 @@ import com.iapps.libs.helpers.BaseKeys
 
 class APIs : BaseKeys() {
     companion object {
-        val BASE_URL = "https://memu.world/api/web/"
+        val BASE_URL = "https://app.memu.world/api/web/"
         val VEHICLE = "vehicle/"
         val BOOKING = "booking/"
         val USER = "user/"
